377e00778ed2829f94951e7c26ba765d91c48aa5
Trace Helms <tracehelms@gmail.com>
2017-04-18 21:36:45 -0600

    add basic movement & make it fluid

--- src/sprites/Pacman.js
+++ src/sprites/Pacman.js
@@ -1,37 +1,92 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
-  constructor ({ game, x, y, asset, cursors}) {
+  constructor ({ game, x, y, asset }) {
     super(game, x, y, asset);
     game.physics.arcade.enable(this);
-    this.body.width = 28;
-    this.body.height = 28;
 
     this.anchor.setTo(0.5);
     this.animations.add("chomp", null, 10, true);
-    this.cursors = cursors;
+    this.direction = null;
+    this.turning = null;
+    this.turnAtX = null;
+    this.turnAtY = null;
   }
 
   update() {
-    if (this.cursors.left.isDown) {
-      this.angle = 180;
-      this.animations.play("chomp");
-      this.body.velocity.setTo(-100, 0);
-    } else if (this.cursors.right.isDown) {
-      this.angle = 0;
-      this.animations.play("chomp");
-      this.body.velocity.setTo(100, 0);
-    } else if (this.cursors.up.isDown) {
-      this.angle = -90;
-      this.animations.play("chomp");
-      this.body.velocity.setTo(0, -100);
-    } else if (this.cursors.down.isDown) {
-      this.angle = 90;
-      this.animations.play("chomp");
-      this.body.velocity.setTo(0, 100);
-    } else {
-      this.animations.stop();
-      this.body.velocity.setTo(0, 0);
+    // console.log("x", this.x);
+    // console.log("y", this.y);
+    // console.log("turning", this.turning);
+    // console.log("turn at x", this.turnAtX);
+    // console.log("turn at y", this.turnAtY);
+    this.move();
+    if (this.turning) {
+      this.turn();
     }
   }
+
+  getDirection() {
+    this.direction;
+  }
+
+  setDirection(direction) {
+    this.direction = direction;
+  }
+
+  move() {
+    switch (this.direction) {
+      case Phaser.UP:
+        this.scale.x = 1;
+        this.angle = -90;
+        this.animations.play("chomp");
+        this.body.velocity.setTo(0, -100);
+        break;
+      case Phaser.DOWN:
+        this.scale.x = 1;
+        this.angle = 90;
+        this.animations.play("chomp");
+        this.body.velocity.setTo(0, 100);
+        break;
+      case Phaser.LEFT:
+        this.scale.x = -1;
+        this.angle = 0;
+        this.animations.play("chomp");
+        this.body.velocity.setTo(-100, 0);
+        break;
+      case Phaser.RIGHT:
+        this.scale.x = 1;
+        this.angle = 0;
+        this.animations.play("chomp");
+        this.body.velocity.setTo(100, 0);
+        break;
+      default:
+        this.animations.stop();
+        this.body.velocity.setTo(0, 0);
+    }
+  }
+
+  turn() {
+    const cx = Math.floor(this.x);
+    const cy = Math.floor(this.y);
+    const threshold = 3;
+
+    if (this.game.math.fuzzyEqual(cx, this.turnAtX, threshold) && this.game.math.fuzzyEqual(cy, this.turnAtY, threshold)) {
+      this.direction = this.turning;
+      this.x = this.turnAtX;
+      this.y = this.turnAtY;
+      this.body.reset(this.turnAtX, this.turnAtY);
+      this.turning = null;
+    }
+  }
+
+  setTurn(direction, x, y) {
+    this.turning = direction;
+    this.turnAtX = x;
+    this.turnAtY = y;
+  }
+
+  turningDirection() {
+    return this.turning;
+  }
+
 }
--- src/states/Game.js
+++ src/states/Game.js
@@ -3,25 +3,32 @@ import Phaser from 'phaser'
 import Pacman from '../sprites/Pacman'
 
 export default class extends Phaser.State {
+  constructor() {
+    super();
+    this.marker = {};
+    this.map = null;
+    this.directions = {};
+    this.cursors = null;
+    this.gridSize = 32;
+  }
   init () {}
   preload () {}
 
   create () {
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    const cursors = this.input.keyboard.createCursorKeys();
+    this.cursors = this.input.keyboard.createCursorKeys();
 
-    let map = this.add.tilemap('level');
-    map.addTilesetImage('pacman_tiles', 'pacman_tiles');
-    this.wallLayer = map.createLayer('walls');
-    map.setCollisionBetween(1, 5000, true, this.wallLayer);
+    this.map = this.add.tilemap('level');
+    this.map.addTilesetImage('pacman_tiles', 'pacman_tiles');
+    this.wallLayer = this.map.createLayer('walls');
+    this.map.setCollisionBetween(1, 5000, true, this.wallLayer);
     this.addTitle();
 
     this.pacman = new Pacman({
       game: this.game,
-      x: 192,
-      y: 224,
-      asset: 'pacman',
-      cursors: cursors
+      x: 208,
+      y: 240,
+      asset: 'pacman'
     });
 
     this.game.add.existing(this.pacman);
@@ -30,11 +37,59 @@ export default class extends Phaser.State {
   update() {
     this.world.wrap(this.pacman, 0, true);
     this.physics.arcade.collide(this.pacman, this.wallLayer);
+
+    this.checkKeys();
   }
 
   render () {
     if (__DEV__) {
       // this.game.debug.body(this.pacman);
+    }
+  }
+
+  checkKeys() {
+    if (this.cursors.up.isDown && this.pacman.getDirection() !== Phaser.UP) {
+      this.checkDirection(Phaser.UP);
+    } else if (this.cursors.down.isDown && this.pacman.getDirection() !== Phaser.DOWN) {
+      this.checkDirection(Phaser.DOWN);
+    } else if (this.cursors.left.isDown && this.pacman.getDirection() !== Phaser.LEFT) {
+      this.checkDirection(Phaser.LEFT);
+    } else if (this.cursors.right.isDown && this.pacman.getDirection() !== Phaser.RIGHT) {
+      this.checkDirection(Phaser.RIGHT);
+    }
+  }
+
+  checkDirection(direction) {
+    if (direction === this.pacman.getDirection() || direction === this.pacman.turningDirection()) {
+      return null;
+    }
+
+    const i = this.wallLayer.index;
+    const x = this.math.snapToFloor(Math.floor(this.pacman.x), 32) / 32;
+    const y = this.math.snapToFloor(Math.floor(this.pacman.y), 32) / 32;
+    let result = null;
+
+    switch (direction) {
+      case Phaser.UP:
+        result = this.map.getTileAbove(i, x, y);
+        break;
+      case Phaser.DOWN:
+        result = this.map.getTileBelow(i, x, y);
+        break;
+      case Phaser.LEFT:
+        result = this.map.getTileLeft(i, x, y);
+        break;
+      case Phaser.RIGHT:
+        result = this.map.getTileRight(i, x, y);
+        break;
+    }
+
+    if (result.index === -1) {
+      const turnPointX = (x * this.gridSize) + (this.gridSize / 2);
+      const turnPointY = (y * this.gridSize) + (this.gridSize / 2);
+
+      // this.pacman.setDirection(direction);
+      this.pacman.setTurn(direction, turnPointX, turnPointY);
     }
   }
 
